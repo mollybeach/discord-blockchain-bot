e1b687dd59df5c5784bf6add71da29f790e25eea
mollybeach <mollyjbeach@gmail.com>
2022-11-17 10:20:57 -0800

    added app use and post functions

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
-const express = require("express");
 const Moralis = require("moralis").default;
+const express = require("express");
 const discord = require("discord.js");
 require("dotenv").config();
-const app = express();
 const port = 3000;
+const app = express();
 
 const client = new discord.Client({
     intents: [],
   });
 
 client.login(process.env.PASS);
+
+app.use(express.json());
+
+app.post("/webhook", async (req, res) => {
+  const { body, headers } = req;
+
+  try {
+    Moralis.Streams.verifySignature({
+      body,
+      signature: headers["x-signature"],
+    });
+
+
+    let from = body.txs[0].fromAddress;
+    let amount = Number(body.txs[0].value / 1E18);
+
+    const channel = await client.channels.fetch(process.env.CHANNEL);
+    channel.send(`A new donation has been submitted by ${from}, for ${amount.toFixed(2)} MATIC!!!!`);
+
+    return res.status(200).json();
+  } catch (e) {
+    console.log("This is Not Moralis");
+    return res.status(400).json();
+  }
+});
